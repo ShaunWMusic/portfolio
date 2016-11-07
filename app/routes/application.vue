edb40972dfbb7ec2fe43c6274d76e2272e204f71
“Shaun <shaunwmusic@gmail.com>
2016-11-06 21:36:29 -0600

    Import bulma, uncomment fa, create app, index and add image

---
+++ app/routes/application.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+  <div class="">
+    <div class="section is-paddingless">
+      <img class="image" src="../public/img/image-7.jpg" alt="">
+      <img class="image" src="../img/image-1.jpg" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+
+  methods: {
+
+  },
+};
+</script>
