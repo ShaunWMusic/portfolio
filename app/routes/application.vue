6e38e8c9e5ca3cfa5648da94c49fb7c6089f3115
“Shaun <shaunwmusic@gmail.com>
2016-11-07 11:13:21 -0600

    Add Media query to change image

--- app/routes/application.vue
+++ app/routes/application.vue
@@ -1,9 +1,10 @@
 <template lang="html">
   <div class="">
     <div class="section is-paddingless">
-      <img class="image" src="../img/image.jpg" alt="">
+      <img class="image image-mobile" src="../img/image.jpg" alt="">
+      <img class="image image-tablet" src="../img/image-1.jpg" alt="">
     <nav class="nav">
-        <h1 class="title nav-item">Shaun Willis | Web Developer | Artist</h1>
+        <h1 class="title nav-item">Shaun Willis | Web Developer || Artist || Worship Leader</h1>
     </nav>
     </div>
   </div>
