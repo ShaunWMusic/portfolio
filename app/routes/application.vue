d8e609ab861434859b04c72268ec4bf4377514a8
“Shaun <shaunwmusic@gmail.com>
2016-11-09 22:02:24 -0600

    Editing portfolio

--- app/routes/application.vue
+++ app/routes/application.vue
@@ -5,18 +5,38 @@
       <img class="image image-tablet" src="../img/image-1.jpg" alt="">
     <nav class="nav">
         <h1 class="nav-item title">Shaun Willis | Web Developer</h1>
-
+        <ul class="nav-center">
+          <li class="link nav-item"><a href="#portfolio">Portfolio</a></li>
+          <li class="link nav-item"><a href="#skills">Skills</li>
+            <li class="link nav-item"><a href="#about">About</li>
+              <li class="link nav-item"><a href="#contact">Contact</li>
+              </ul>
     </nav>
       <div class="section">
-        <h2 class="title has-text-centered portfolio">Portfolio</h2>
-      <div class="control">
+        <h2 class="title has-text-centered portfolio" id="portfolio">Portfolio</h2>
+    <div class="columns">
+      <div class="column is-one-third">
+          <div class="control">
           <a href="https://github.com/ShaunWMusic/17-puppy-adoption/tree/develop"><img class="gif gif-mobile" src="/img/puppies.gif" alt=""></a>
         <a href="https://github.com/ShaunWMusic/17-puppy-adoption/tree/develop"><img class="gif gif-desktop" src="/img/dpuppies.gif" alt=""></a>
+        </div>
       </div>
-      <div class="control">
-          <a href="https://github.com/ShaunWMusic/17-puppy-adoption/tree/develop"><img class="gif gif-mobile gif-boo" src="/img/boo.gif" alt=""></a>
-        <a href="https://github.com/ShaunWMusic/17-puppy-adoption/tree/develop"><img class="gif gif-desktop gif-boo" src="/img/dboo.gif" alt=""></a>
+    <div class="column">
+        <div class="control">
+          <a href="https://github.com/ShaunWMusic/16-monster-mash/tree/develop"><img class="gif gif-mobile gif-boo" src="/img/boo.gif" alt=""></a>
+        <a href="https://github.com/ShaunWMusic/16-monster-mash/tree/develop"><img class="gif gif-desktop gif-boo" src="/img/dboo.gif" alt=""></a>
       </div>
+    </div>
+      </div>
+    </div>
+      <div class="section">
+          <h2 class="title has-text-centered portfolio" id="skills">Skills</h2>
+      </div>
+      <div class="section">
+          <h2 class="title has-text-centered portfolio" id="about">About</h2>
+      </div>
+      <div class="section">
+          <h2 class="title has-text-centered portfolio" id="contact">Contact</h2>
       </div>
     </div>
   </div>
