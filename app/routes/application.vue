e53d494cd0acd3d4ef958e5d9933fef907668150
“Shaun <shaunwmusic@gmail.com>
2016-11-13 18:41:00 -0600

    Changing layout

--- app/routes/application.vue
+++ app/routes/application.vue
@@ -13,6 +13,25 @@
           <li class="link nav-item"><a href="#contact">Contact</li>
         </ul>
       </nav>
+
+      </div>
+      <div class="section section-about">
+        <div class="container">
+          <h2 class="title has-text-centered" id="about"></h2>
+            <div class="about">
+              <img class="about-img"src="./img/image-6.jpg" alt="">
+              <div class="about-content">
+              <h4 class="title">HELLO,</h4>
+                <p>I'm Shaun Willis. This is my design portfolio where I share
+                     all of my favorite work. When I'm not developing, I enjoy spending
+                     time with my family, leading worship at my church, drinking
+                      coffee with my wife, eating pb sandwiches with my son
+                      and more!
+                      If you would like to follow me on Twitter. My username is <a href="/http://twitter.com/shaunwmusic">@Shaunwmusic</a>.</p>
+              </div>
+            </div>
+        </div>
+      </div>
       <div class="section">
         <h2 class="title has-text-centered portfolio" id="portfolio">Portfolio</h2>
     <div class="columns is-desktop is-mobile">
@@ -30,71 +49,24 @@
     </div>
       </div>
     </div>
-      <div class="section section-skills">
-        <div class="container">
-            <h2 class="title has-text-centered" id="skills">Skills</h2>
-        <div class="columns is-mobile">
-            <div class="column skills-content">
-            <h4 class="title">Web Development</h4>
-            <p>Html,CSS,Vanilla JavaScript,Vue.js,Github,Git,NPM,Yarn,Adonis.js,Node.js,
-            Heroku,Bulma,Flexbox(CSS),Float(CSS)</p>
-          </div>
-          <div class="column skills-content">
-            <h4 class="title">Dabble In</h4>
-            <p>Sketch,Adobe Photoshop,</p>
-          </div>
-        </div>
-        </div>
-      </div>
-      <div class="section section-about">
-        <div class="container">
-          <h2 class="title has-text-centered" id="about">About</h2>
-            <div class="about">
-              <img class="about-img"src="./img/image-6.jpg" alt="">
-              <div class="about-content">
-              <h4 class="title">HELLO,</h4>
-                <p>I'm Shaun Willis. This is my design portfolio where I share
-                     all of my favorite work. When I'm not developing, I enjoy spending
-                     time with my family, leading worship at my church, drinking
-                      coffee with my wife, eating pb sandwiches with my son
-                      and more!
-                      If you would like to follow me on Twitter. My username is <a href="/http://twitter.com/shaunwmusic">@Shaunwmusic</a>.</p>
-              </div>
-            </div>
-        </div>
-      </div>
       <div class="section">
           <div class="container">
-            <h2 class="title has-text-centered" id="contact">Contact</h2>
+            <h2 class="title has-text-centered" id="contact"></h2>
             <div class="about">
-              <div class="general-information">
-                <h2 class="title">General Information</h2>
-                <p>I am currently enrolled at Iron Yard in Nashville, and I will
-                  be attending their Front-End Developers' Course in September.</p>
-                <p>I will be pursuing design work after December 16th, 2016.</p>
-                <p>Please only use phone contact for urgent inquiries. Otherwise, Twitter and email
-                  are the best way to reach me.</p>
-              </div>
-              <div class="contact-details">
-                <h2 class="title">Contact Details</h2>
-              <p><span class="fa fa-1x fa-phone"></span><a href="tel:615-556-3930">615-556-3930</a></p>
-              <p><span class="fa fa-1x fa-envelope"></span><a href="mailto:shaunwmusic@gmail.com">shaunwmusic@gmail.com</a></p>
-              <p><span class="fa fa-1x fa-twitter"></span><a href="https://www.twitter.com/intent/tweet?screen_name=shaunwmusic">@shaunwmusic</a></p>
+              <div class="contact-details has-text-centered">
+
+              <span class="fa fa-phone"></span><a href="tel:615-556-3930"></a>
+              <span class="fa fa-envelope"></span><a href="mailto:shaunwmusic@gmail.com"></a>
+              <span class="fa fa-twitter"></span><a href="https://www.twitter.com/intent/tweet?screen_name=shaunwmusic"></a>
+              <span class="fa fa-facebook"><a href="http://facebook.com/shaunwmusic"></a></span>
+              <span class="fa fa-linkedin"><a href="https://www.linkedin.com/in/shaunwillis"></a></span>
+              <span class="fa fa-github"><a href="https://www.github.com/shaunwmusic"></a></span>
+              <p>© Shaun Willis.</p>
               </div>
             </div>
           </div>
       </div>
-      <div class="section">
-        <div class="container">
-          <div class="footer is-primary has-text-centered">
-            <span class="fa fa-twitter"><a href="http://twitter.com/shaunwmusic"></a></span>
-            <span class="fa fa-facebook"><a href="http://facebook.com/shaunwmusic"></a></span>
-            <span class="fa fa-linkedin"><a href="https://www.linkedin.com/in/shaunwillis"></a></span>
-            <span class="fa fa-github"><a href="https://www.github.com/shaunwmusic"></a></span>
-              <p>© Shaun Willis.</p>
-          </div>
-        </div>
-      </div>
+
     </div>
   </div>
 </template>
