1d61662946df244ad27ca941f38aa2c751073ee7
“Shaun <shaunwmusic@gmail.com>
2016-11-18 15:39:49 -0600

    feat: Add the second iphone image

--- app/routes/application.vue
+++ app/routes/application.vue
@@ -35,7 +35,7 @@
       </div>
       <div class="section">
         <h2 class="title has-text-centered portfolio" id="portfolio">Portfolio</h2>
-    <div class="columns is-desktop is-mobile">
+    <div class="columns columns-portfolio is-desktop is-mobile">
       <div class="column">
           <div class="control">
           <a href="https://github.com/ShaunWMusic/17-puppy-adoption/tree/develop"><img class="gif gif-mobile" src="/img/puppies.gif" alt=""></a>
@@ -47,6 +47,7 @@
         <div class="control">
           <a href="https://github.com/ShaunWMusic/16-monster-mash/tree/develop"><img class="gif gif-mobile gif-boo" src="/img/boo.gif" alt=""></a>
         <a href="https://github.com/ShaunWMusic/16-monster-mash/tree/develop"><img class="gif gif-desktop gif-boo" src="/img/dboo.gif" alt=""></a>
+        <img class="iphone" src="./img/iphonepic2.png" alt="" />
       </div>
     </div>
       </div>
